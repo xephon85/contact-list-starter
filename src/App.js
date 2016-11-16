930d0ae076b69c1c2810bfd1a9fb15270129b4b6
Colin Clarkson <colinsclarkson@gmail.com>
2016-11-15 20:13:47 -0600

    added contact list, added filters

--- src/App.js
+++ src/App.js
@@ -1,10 +1,71 @@
 import React, { Component } from 'react';
+import ContactList from './ContactList';
+import SearchBar from './SearchBar';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchText: '',
+      contacts: [
+        {
+          _id: 'dale',
+          name: 'Dale Cooper',
+          occupation: 'FBI Agent',
+          avatar: 'https://upload.wikimedia.org/wikipedia/en/5/50/Agentdalecooper.jpg'
+        },
+        {
+          _id: 'spike',
+          name: 'Spike Spiegel',
+          occupation: 'Bounty Hunter',
+          avatar: 'http://vignette4.wikia.nocookie.net/deadliestfiction/images/d/de/Spike_Spiegel_by_aleztron.jpg/revision/latest?cb=20130920231337'
+        },
+        {
+          _id: 'wirt',
+          name: 'Wirt',
+          occupation: 'adventurer',
+          avatar: 'http://66.media.tumblr.com/5ea59634756e3d7c162da2ef80655a39/tumblr_nvasf1WvQ61ufbniio1_400.jpg'
+        },
+        {
+          _id: 'michael',
+          name: 'Michael Myers',
+          occupation: 'Lovng little brother',
+          avatar: 'http://vignette2.wikia.nocookie.net/villains/images/e/e3/MMH.jpg/revision/latest?cb=20150810215746'
+        },
+        {
+          _id: 'dana',
+          name: 'Dana Skully',
+          occupation: 'FBI Agent',
+          avatar: 'https://pbs.twimg.com/profile_images/718881904834056192/WnMTb__R.jpg'
+        }
+      ]
+    };
+  }
+
+  handleSearchBarChange(event) {
+    this.setState({
+      contacts: this.state.contacts,
+      searchText: event.target.value
+    })
+  }
+
+  getFilteredContacts() {
+    const term = this.state.searchText.trim().toLowerCase();
+    const contacts = this.state.contacts;
+    if (!term) {
+      return contacts;
+    }
+    return contacts.filter(contact => {
+      return contact.name.toLowerCase().search(term) >= 0;
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>Contact List!</h1>
+        <SearchBar value={this.state.searchText} onChange={this.handleSearchBarChange.bind(this)}/>
+        <ContactList contacts={this.getFilteredContacts()} />
       </div>
     );
   }
